d9359ecea9cdf3ee4614af078f3b9d403629106c
michaelsilvarj <michaelsilv@gmail.com>
2022-05-03 01:58:47 -0300

    Adição de figura sgv de erro

--- script/script.js
+++ script/script.js
@@ -3,22 +3,25 @@ let botao = document.querySelector('.btn-enviar');
 let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 let send = document.getElementById("ok");
+let erro = document.getElementById("erro");
 let enviar = true;
 
 
 function validador(){
     if (enviar !==true){
-      
         send.classList.remove('valido');
         send.classList.add('invalido');
-        
-      
+        erro.classList.add('erro--validacao');
+        document.querySelector('.mensagem').innerHTML = 'Please provide a valid email';
+
 
     }else if (enviar ===true) {
         send.classList.remove('invalido');
-        send.classList.add('valido');
-        
-        
+        send.classList.add('valido'); 
+        erro.classList.remove('erro--validacao'); 
+        erro.classList.add('sucesso--validacao');
+        document.querySelector('.mensagem').innerHTML = ''; 
+          
     }
 }
 
